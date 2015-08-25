80437ef1108de0df2baa526f57755a5be767db62
Pelatos <mattia.giurato@gmail.com>
2015-08-25 18:57:19 +0300

    MAdgwick algorithm implemented

--- IMU_read.m
+++ IMU_read.m
@@ -4,22 +4,16 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all
 close all 
-clc
+% clc
 
 %% Parameters definition
 Parameters
 
 %% Import logged data
-RAW = dlmread('log_raw_2.txt');
-acc_x = RAW(:,1);
-acc_y = RAW(:,2);
-acc_z = RAW(:,3);
-gyro_x = RAW(:,4);
-gyro_y = RAW(:,5);
-gyro_z = RAW(:,6);
-mag_x = RAW(:,7);
-mag_y = RAW(:,8);
-mag_z = RAW(:,9);
+RAW = dlmread('log_raw_3.txt');
+acc = RAW(:,1:3);
+gyro = RAW(:,4:6);
+mag = RAW(:,7:9);
 
 %% Getting information from logged data
 fs = 50;                   %[Hz]
@@ -33,30 +27,30 @@ t = 0 : dt : (length(RAW)*dt - dt);
 
 %% Plot RAW data
 % figure('name','Accelerometer')
-% plot(t, acc_x)
+% plot(t, acc(:,1))
 % hold on
-% plot(t, acc_y)
-% plot(t, acc_z)
+% plot(t, acc(:,2))
+% plot(t, acc(:,3))
 % hold off
 % legend('X_{body}', 'Y_{body}', 'Z_{body}')
 % title('Accelerometer RAW data')
 % grid
 % 
 % figure('name','Gyroscope')
-% plot(t, gyro_x)
+% plot(t, gyro(:,1))
 % hold on
-% plot(t, gyro_y)
-% plot(t, gyro_z)
+% plot(t, gyro(:,2))
+% plot(t, gyro(:,3))
 % hold off
 % legend('p', 'q', 'r')
 % title('Gyroscope RAW data')
 % grid
 % 
 % figure('name','Magnetometer')
-% plot(t, mag_x)
+% plot(t, mag(:,1))
 % hold on
-% plot(t, mag_y)
-% plot(t, mag_z)
+% plot(t, mag(:,2))
+% plot(t, mag(:,3))
 % hold off
 % legend('X_{body}', 'Y_{body}', 'Z_{body}')
 % title('Magnetometer RAW data')
@@ -68,147 +62,140 @@ LPF = designfilt('lowpassfir','PassbandFrequency',0.10, ...
       'StopbandAttenuation',65,'DesignMethod','kaiserwin');
 % fvtool(LPF)
 
-acc_xf = filtfilt(LPF,acc_x);
-acc_yf = filtfilt(LPF,acc_y);
-acc_zf = filtfilt(LPF,acc_z);
+acc_f = filtfilt(LPF,acc);
 
-gyro_xf = filtfilt(LPF,gyro_x);
-gyro_yf = filtfilt(LPF,gyro_y);
-gyro_zf = filtfilt(LPF,gyro_z);
+gyro_f = filtfilt(LPF,gyro);
  
 % figure('name', 'test')
-% plot(t, acc_y)
+% plot(t, acc)
 % hold on
-% plot(t, acc_yf)
+% plot(t, acc_f)
 % hold off
 % grid minor
-% 
+
 % figure('name','Accelerometer')
-% plot(t, acc_xf)
+% plot(t, acc_f(:,1))
 % hold on
-% plot(t, acc_yf)
-% plot(t, acc_zf)
+% plot(t, acc_f(:,2))
+% plot(t, acc_f(:,3))
 % hold off
 % title('Accelerometer filtered data')
 % legend('X_{body}', 'Y_{body}', 'Z_{body}')
 % grid
 % 
 % figure('name','Gyroscope')
-% plot(t, gyro_xf)
+% plot(t, gyro_f(:,1))
 % hold on
-% plot(t, gyro_yf)
-% plot(t, gyro_zf)
+% plot(t, gyro_f(:,2))
+% plot(t, gyro_f(:,3))
 % hold off
 % legend('p', 'q', 'r')
 % title('Gyroscope filtered data')
 % grid
 
 %% Calibrating accelerometer
-acc_max_x = max(acc_xf);
-acc_max_y = max(acc_yf);
-acc_max_z = max(acc_zf);
 
-acc_min_x = min(acc_xf);
-acc_min_y = min(acc_yf);
-acc_min_z = min(acc_zf);
+% Find gains and biases
+bias_a_guess = .5;
+gain_a_guess = 9.81/1000;
 
-acc_x_os = (acc_min_x + acc_max_x)/2;
-acc_y_os = (acc_min_y + acc_max_y)/2;
-acc_z_os = (acc_min_z + acc_max_z)/2;
+optionsOpt = optimset('LargeScale', 'off', 'Display', 'off', 'TolX', 1E-21, 'TolFun', 1E-21, 'HessUpdate', 'bfgs', 'MaxIter', 128);  
+optVal = [ones(1,3)*bias_a_guess ones(1,3)*gain_a_guess];  	% vector of initial guess for optimal value
+optValScaler = 1 ./ optVal;                                 % individual scalers unit optimal values
+optVal = optVal .* optValScaler;                            % initial guess for optimal values = unity
+optVal = fminunc('objFunAccelMag', optVal, optionsOpt, optValScaler, acc_f, 9.81);
+optVal = optVal ./ optValScaler;                            % rescale optimal values to original units
+bias_a = optVal(1:3);
+gain_a = optVal(4:6);
 
-acc_x_dp = acc_xf - acc_x_os;
-acc_y_dp = acc_yf - acc_y_os;
-acc_z_dp = acc_zf - acc_z_os;
-
-%PLOT THE RESULTS
-disp('The estimated accelerometer offset is:')
-disp(['X:', num2str(acc_x_os)])
-disp(['Y:', num2str(acc_y_os)])
-disp(['Z:', num2str(acc_z_os)])
-
-%% Calibrating magnetometer
-
-%Hard iron errors
-mag_max_x = max(mag_x);
-mag_max_y = max(mag_y);
-mag_max_z = max(mag_z);
-
-mag_min_x = min(mag_x);
-mag_min_y = min(mag_y);
-mag_min_z = min(mag_z);
-
-mag_x_os = (mag_min_x + mag_max_x)/2;
-mag_y_os = (mag_min_y + mag_max_y)/2;
-mag_z_os = (mag_min_z + mag_max_z)/2;
-
-mag_x_dp = mag_x - mag_x_os;
-mag_y_dp = mag_y - mag_y_os;
-mag_z_dp = mag_z - mag_z_os;
-
-%Soft iron errors
-% Average distance from the centre
-avg_x_radius = (max(mag_x_dp) + (-1)*min(mag_x_dp))/2;
-avg_y_radius = (max(mag_y_dp) + (-1)*min(mag_y_dp))/2;
-avg_z_radius = (max(mag_z_dp) + (-1)*min(mag_z_dp))/2;  
-
-avg_radius = (avg_x_radius + avg_y_radius + avg_z_radius)/3;
-
-% Scale factor 
-mag_x_scale = avg_radius/avg_x_radius;
-mag_y_scale = avg_radius/avg_y_radius;
-mag_z_scale = avg_radius/avg_z_radius;
-
-%PLOT THE RESULTS
-disp('The estimated magnetometer offset is:')
-disp(['X:', num2str(mag_x_os)])
-disp(['Y:', num2str(mag_y_os)])
-disp(['Z:', num2str(mag_z_os)])
-disp('The estimated magnetometer scale factors are:')
-disp(['X:', num2str(mag_x_scale)])
-disp(['Y:', num2str(mag_y_scale)])
-disp(['Z:', num2str(mag_z_scale)])
-
-%% 3D PLOT
-%Compensating Accelerometer and Magnetometer
-acc_xc = acc_xf - acc_x_os;
-acc_yc = acc_yf - acc_y_os;
-acc_zc = acc_zf - acc_z_os;
-
-mag_xc = (mag_x - mag_x_os) * mag_x_scale;
-mag_yc = (mag_y - mag_y_os) * mag_y_scale;
-mag_zc = (mag_z - mag_z_os) * mag_z_scale;
-
-
-figure('name','Accelerometer')
-plot3(acc_xf, acc_yf, acc_zf,'.')
-hold on
-plot3(acc_xc, acc_yc, acc_zc)
-axis equal
-grid
-
-figure('name','Magnetometer')
-plot3(mag_x, mag_y, mag_z)
-hold on
-plot3(mag_xc, mag_yc, mag_zc)
-axis equal
-grid
-
-figure('Position',[10,40,1024,600]);
+% Plot calibrated data
+figure('name','Accelerometer Calibration');
 subplot(3,1,1:2)
     hold on;    
-    plot(1:length(acc_xf), acc_xf, 'b');
-    plot(1:length(acc_yf), acc_yf, 'r');
-    plot(1:length(acc_zf), acc_zf, 'g');
+    acc_c(:,1) = gain_a(1) * acc_f(:,1) - bias_a(1);
+    acc_c(:,2) = gain_a(2) * acc_f(:,2) - bias_a(2);
+    acc_c(:,3) = gain_a(3) * acc_f(:,3) - bias_a(3);
+    plot(1:length(acc_c), acc_c(:,1), 'b');
+    plot(1:length(acc_c), acc_c(:,2), 'r');
+    plot(1:length(acc_c), acc_c(:,3), 'g');
     legend('X', 'Y', 'Z');
     title('Accelerometer calibration');
     ylabel('Sensor units');
 subplot(3,1,3)    
     hold on;
-    plot(1:length(acc_xf), sqrt((acc_xf.^2) + (acc_yf.^2) + (acc_zf.^2)), 'Color', [0.6, 0.6, 0.6]);
-    plot([0 length(acc_xf)], [1000 1000], 'k:');
-    legend('Feild magnitude', 'Mean feild magnitude');
-    ylabel('Sensor units');
+    plot(1:length(acc_c), sqrt((acc_c(:,1).^2) + (acc_c(:,2).^2) + (acc_c(:,3).^2)), 'Color', [0.6, 0.6, 0.6]);
+    plot([0 length(acc_c)], [9.81 9.81], 'k:');
+    legend('Measured g', 'g');
+    ylabel('[m/s^2]');
     xlabel('Sample');
 drawnow;
+
+%Print gains and biases
+disp('The estimated Accelerometer biases are:')
+disp(['X:', num2str(bias_a(1))])
+disp(['Y:', num2str(bias_a(2))])
+disp(['Z:', num2str(bias_a(3))])
+disp('The estimated Accelerometer scale factors are:')
+disp(['X:', num2str(gain_a(1))])
+disp(['Y:', num2str(gain_a(2))])
+disp(['Z:', num2str(gain_a(3))])
+
+%% Calibrating magnetometer
+
+% Find gains and biases
+bias_m_guess = 0.2;
+gain_m_guess = 1/360;
+
+optionsOpt = optimset('LargeScale', 'off', 'Display', 'off', 'TolX', 1E-21, 'TolFun', 1E-21, 'HessUpdate', 'bfgs', 'MaxIter', 128);  
+optVal = [ones(1,3)*bias_m_guess ones(1,3)*gain_m_guess];  	% vector of initial guess for optimal value
+optValScaler = 1 ./ optVal;                                 % individual scalers unit optimal values
+optVal = optVal .* optValScaler;                            % initial guess for optimal values = unity
+optVal = fminunc('objFunAccelMag', optVal, optionsOpt, optValScaler, mag, 1);
+optVal = optVal ./ optValScaler;                            % rescale optimal values to original units
+bias_m = optVal(1:3);
+gain_m = optVal(4:6);
+
+% Plot calibrated data
+figure('name','Magnetometer Calibration');
+subplot(3,1,1:2)
+    hold on;    
+    mag_c(:,1) = gain_m(1) * mag(:,1) - bias_m(1);
+    mag_c(:,2) = gain_m(2) * mag(:,2) - bias_m(2);
+    mag_c(:,3) = gain_m(3) * mag(:,3) - bias_m(3);
+    plot(1:length(mag_c), mag_c(:,1), 'b');
+    plot(1:length(mag_c), mag_c(:,2), 'r');
+    plot(1:length(mag_c), mag_c(:,3), 'g');
+    legend('X', 'Y', 'Z');
+    title('Magnetometer calibration');
+    ylabel('Sensor units');
+subplot(3,1,3)    
+    hold on;
+    plot(1:length(mag_c), sqrt((mag_c(:,1).^2) + (mag_c(:,2).^2) + (mag_c(:,3).^2)), 'Color', [0.6, 0.6, 0.6]);
+    plot([0 length(mag_c)], [1 1], 'k:');
+    legend('Measured field', 'field');
+    ylabel('[]');
+    xlabel('Sample');
+drawnow;
+
+%Print gains and biases
+disp('The estimated Magnetometer biases are:')
+disp(['X:', num2str(bias_m(1))])
+disp(['Y:', num2str(bias_m(2))])
+disp(['Z:', num2str(bias_m(3))])
+disp('The estimated Magnetometer scale factors are:')
+disp(['X:', num2str(gain_m(1))])
+disp(['Y:', num2str(gain_m(2))])
+disp(['Z:', num2str(gain_m(3))])
+
+%% 3D PLOT
+% figure('name','Accelerometer')
+% plot3(acc_c(:,1), acc_c(:,2), acc_c(:,3))
+% axis equal
+% grid
+% 
+% figure('name','Magnetometer')
+% plot3(mag_c(:,1), mag_c(:,2), mag_c(:,3))
+% axis equal
+% grid
 
 %% End of code
